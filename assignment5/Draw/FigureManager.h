bbb6f26603702bf2507414dfde1362366aae8712
ViSG <esecpp2016@163.com>
2016-11-17 16:35:44 +0800

    Add files via upload

---
+++ assignment5/Draw/FigureManager.h
@@ -0,0 +1,29 @@
+#ifndef _FIGUREMANAGER_H_
+#define _FIGUREMANAGER_H_
+
+#include <iostream>
+
+class BlackBoard; 
+
+class FigureManager
+{
+public:
+	static FigureManager &handle()
+	{
+		static FigureManager manager; 
+		return manager; 
+	}
+    
+    // FigureManager类析构函数
+    virtual ~FigureManager() { }
+ 
+    // FigureManager类接口.
+public:
+	void input(std::istream &is); 
+    void display(BlackBoard &board); 
+
+}; // class FigureManager类定义结束.
+
+void InitiateFigureManager(); 
+
+#endif // #ifndef _FIGUREMANAGER_H_
